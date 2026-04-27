dbb8ba30c52a6381a9ca7bd6e28c7f8ed19db898
Farooqtd1 <farooqaliqarrar00@gmail.com>
2026-04-28 01:02:05 +0500

    Add files via upload

---
+++ index.cjs
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello Farooq bahi! Server is running 🚀');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
